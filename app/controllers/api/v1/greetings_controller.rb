1fc5458e7c113a09426a0349714c049d033ec6b3
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-09-19 04:32:27 -0600

    Add end point for api

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def random
+        @greeting = Greeting.order("RANDOM()").first
+        render json: { greeting: @greeting.content }
+      end
+    end
+  end
+end
