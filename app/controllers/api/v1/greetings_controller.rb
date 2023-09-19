ab82bf7d2063566d739816d931016a9ed193dcbf
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-09-19 04:45:11 -0600

    Add linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class GreetingsController < ApplicationController
       def random
-        @greeting = Greeting.order("RANDOM()").first
+        @greeting = Greeting.order('RANDOM()').first
         render json: { greeting: @greeting.content }
       end
     end
